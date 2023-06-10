219d56d77c3e15fbac15213234637e0ac700a1a5
dezzer <baev2m@gmail.com>
2023-06-10 22:23:51 +0500

    feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Drawboard',
+        short_name: 'Drawboard',
+        description: 'Drawboard application with lots of functions',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
+    port: 3000,
+  },
+
+  preview: {
     port: 3000,
   },
 
